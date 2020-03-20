0df8d6ebf8c9232cdee804d72323ba24b332212d
TitusWongCN <wzhwno1@163.com>
2020-03-20 16:02:20 +0800

    Optimize for all kind of msgs

--- GroupMonitorForFF/recorder.py
+++ GroupMonitorForFF/recorder.py
@@ -1,6 +1,6 @@
 # -*- coding=utf-8 -*-
 # python35
-from GroupMonitorForFF.utils import *
+from utils import *
 from itchat.content import *
 import itchat
 import os
@@ -10,30 +10,35 @@ config_file = './config.ini'
 # isGroupChat=True表示为群聊消息
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isGroupChat=True)
 def group_reply(msg):
-    timestamp = get_timestamp()
-    sender_name = msg['ActualNickName'] if msg['ActualNickName'] != '' else msg['User']['Self']['NickName']
     group_name = msg['User']['NickName']
-    msg_content = msg['Text']
     settings = get_config(file=config_file)
     group_enabled = settings['global']['group_enabled'].split(',')
     for group in group_enabled:
         if group in group_name or group_name in group:
+            timestamp = get_timestamp()
+            sender_name = msg['ActualNickName'] if msg['ActualNickName'] != '' else msg['User']['Self']['NickName']
+            type = msg['Type'].upper()
+            if type in ['TEXT', 'CARD', 'NOTE']:
+                msg_content = msg['Content']
+            elif type == 'MAP':
+                msg_content = msg['Content'].split(':')[0] + '\t' + msg['Url']
+            else:
+                msg_content = msg['Text'] + '\t' + msg['Content']
             handle_new_folder(group_name)
             file = os.path.join('./records/{}/{}.log'.format(group_name, get_date()))
-            content = '{}\t{}:\t{}\n'.format(timestamp, sender_name, msg_content)
+            content = '{}\t{}\t{}:\t{}\n'.format(timestamp, sender_name, type, msg_content)
             record(file, content)
-
 
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
 def group_file_reply(msg):
-    timestamp = get_timestamp()
-    sender_name = msg['ActualNickName'] if msg['ActualNickName'] != '' else msg['User']['Self']['NickName']
     group_name = msg['User']['NickName']
     settings = get_config(file=config_file)
     group_enabled = settings['global']['group_enabled'].split(',')
     for group in group_enabled:
         if group in group_name or group_name in group:
+            timestamp = get_timestamp()
+            sender_name = msg['ActualNickName'] if msg['ActualNickName'] != '' else msg['User']['Self']['NickName']
             handle_new_folder(group_name)
             download_file = './records/{}/files/{}_'.format(group_name, timestamp) + msg.fileName
             msg.download(download_file)
